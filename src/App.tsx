558216ec230d531c96948236c899bd39c21a29a6
gascheckking <warpaiactivity1@gmail.com>
2025-12-16 17:40:48 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { auth, db, APP_ID } from './firebase';
 import { useState, useEffect } from 'react';
-import { DocumentData } from 'firebase/firestore';
 import { signInAnonymously, onAuthStateChanged } from 'firebase/auth';
 import {
   collection,
