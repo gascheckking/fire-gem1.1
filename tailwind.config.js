80b0e2c0ef81dc7f31ccad1335fc1645ded1ef40
gascheckking <warpaiactivity1@gmail.com>
2025-12-17 21:23:25 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-// tailwind.config.js  (NY FIL)
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: { extend: {} },
   plugins: [],
 };
